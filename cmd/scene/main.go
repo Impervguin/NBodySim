7e132600c7c0d4f329df26fd047823a1bf0b09d9
impervguin <leon53000@yandex.ru>
2024-11-05 13:29:29 +0300

    by random found beatuiful 4 body configuration

--- cmd/scene/main.go
+++ cmd/scene/main.go
@@ -22,12 +22,13 @@ import (
 
 func main() {
 
-	read, _ := reader.NewObjReader("/home/impervguin/Projects/NBodySim/models/20_icosahedron.obj")
+	read, _ := reader.NewObjReader("/home/impervguin/Projects/NBodySim/models/8_octahedron.obj")
 	dir := builder.NewPolygonObjectDirector(&builder.ClassicPolygonFactory{}, read)
 	cube, err := dir.Construct()
 	if err != nil {
 		panic(err)
 	}
+	cube.Transform(transform.NewMoveAction(vector.NewVector3d(0, 0, 50)))
 
 	read, _ = reader.NewObjReader("/home/impervguin/Projects/NBodySim/models/8_octahedron.obj")
 	dir = builder.NewPolygonObjectDirector(&builder.ClassicPolygonFactory{}, read)
@@ -35,7 +36,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	cube2.Transform(transform.NewMoveAction(vector.NewVector3d(-50, 0, 0)))
+	cube2.Transform(transform.NewMoveAction(vector.NewVector3d(50, 0, 0)))
 
 	read, _ = reader.NewObjReader("/home/impervguin/Projects/NBodySim/models/8_octahedron.obj")
 	dir = builder.NewPolygonObjectDirector(&builder.ClassicPolygonFactory{}, read)
@@ -43,8 +44,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	cube3.Transform(transform.NewMoveAction(vector.NewVector3d(0, 40, -30)))
+	cube3.Transform(transform.NewMoveAction(vector.NewVector3d(0, 0, -50)))
 	// fmt.Println(cube3.GetCenter())
+
+	read, _ = reader.NewObjReader("/home/impervguin/Projects/NBodySim/models/8_octahedron.obj")
+	dir = builder.NewPolygonObjectDirector(&builder.ClassicPolygonFactory{}, read)
+	cube4, err := dir.Construct()
+	if err != nil {
+		panic(err)
+	}
+	cube4.Transform(transform.NewMoveAction(vector.NewVector3d(-50, 0, 0)))
 
 	cam := object.NewCamera(
 		*vector.NewVector3d(0, 0, -100),
@@ -55,9 +64,10 @@ func main() {
 
 	sim := simulation.NewSimulation()
 	sim.SetCamera(cam)
-	sim.AddObject(cube, *vector.NewVector3d(0, 0, 0), 10000000000)
-	sim.AddObject(cube2, *vector.NewVector3d(0, 0, 0), 10000000000)
-	sim.AddObject(cube3, *vector.NewVector3d(0, 0, 0), 10000000000)
+	sim.AddObject(cube, *vector.NewVector3d(0.2, 0, 0), 100000000000)
+	sim.AddObject(cube2, *vector.NewVector3d(0, 0, -.2), 100000000000)
+	sim.AddObject(cube3, *vector.NewVector3d(-.2, 0, 0), 100000000000)
+	sim.AddObject(cube4, *vector.NewVector3d(0, 0, .2), 100000000000)
 	sim.SetDt(0.00001)
 
 	myApp := app.New()
@@ -70,7 +80,7 @@ func main() {
 	go func() {
 		time.Sleep(time.Second)
 		width, height = float64(width)*float64(myWindow.Canvas().Scale()), float64(height)*float64(myWindow.Canvas().Scale())
-		cam.Transform(transform.NewRotateAction(vector.NewVector3d(math.Pi/4, -math.Pi/4, 0)))
+		cam.Transform(transform.NewRotateAction(vector.NewVector3d(-math.Pi/4, -math.Pi/4, 0)))
 		drawerfac := objectdrawer.NewSimpleObjectDrawerFabric(mapper.NewSimpleZmapperFabric(int(width), int(height), color.White), approximator.NewFlatApproximatorFabtic())
 		conv := conveyer.NewSimulationConveyer(
 			drawerfac,
@@ -82,7 +92,7 @@ func main() {
 		for {
 			time.Sleep(time.Millisecond * 17)
 			sim.UpdateFor(1)
-			cam.Transform(transform.NewRotateAction(vector.NewVector3d(0, math.Pi/60, 0)))
+			// cam.Transform(transform.NewRotateAction(vector.NewVector3d(0, math.Pi/60, 0)))
 			conv.Convey()
 			myWindow.Content().Refresh()
 		}
